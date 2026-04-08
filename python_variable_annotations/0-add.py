f2da49a7b98b9b1aff053a5ac1d18178d9577e81
titomundo <tito.alberto.mundo@gmail.com>
2026-04-07 21:30:53 -0600

    create python_variable_annotations dir

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def add(a: float, b: float) -> float:
+    """Sums two floats
+
+    Args:
+        a (float): first float
+        b (float): second float
+
+    Returns:
+    float: The sum of a and b
+    """
+    return a + b
---
+++ python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+Duck typing in python
