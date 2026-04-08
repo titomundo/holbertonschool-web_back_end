c39a9c369645952de545a483e7417cfb4494512b
titomundo <tito.alberto.mundo@gmail.com>
2026-04-07 21:44:25 -0600

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""2-floor
+Get a floored float
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float number
+
+    Args:
+    n (float): float number to be floored
+
+    Returns:
+    int: Floored result of n
+    """
+    return math.floor(n)
