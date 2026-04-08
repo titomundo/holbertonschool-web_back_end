39847a2454339852987e0c2ee078f8f365c49b75
titomundo <tito.alberto.mundo@gmail.com>
2026-04-07 21:38:18 -0600

    task 2

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""1-concat:
+Concatenates two string arguments and return the new string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings
+
+    Args:
+    str1 (str): First string
+    str2 (str): Second string
+
+    Returns:
+    str: New string created by joining str1 and str2
+    """
+    return str1 + str2
