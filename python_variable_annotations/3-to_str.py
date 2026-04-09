8c3ae2efefc0fdfaf72f1b9eed8ff595b4a9ffd6
titomundo <tito.alberto.mundo@gmail.com>
2026-04-08 23:49:38 -0600

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""3-to_str
+Converts a float to a string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns a string converted from a float
+
+    Args:
+    n (float): float number to convert
+
+    Returns:
+    str: String of n
+    """
+    return str(n)
