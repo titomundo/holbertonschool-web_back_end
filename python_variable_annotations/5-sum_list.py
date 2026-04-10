23e6cd885fc9fcd84ad1ce601d73221a27b399e7
titomundo <tito.alberto.mundo@gmail.com>
2026-04-09 21:24:32 -0600

    task % 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""5-sum_list:
+Get the sum of a list of floats
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Returns the sum of a list of floats
+
+    Args:
+    input_list (list[float]): list of floats
+
+    Returns:
+    float: Sum of floats
+    """
+    return sum(input_list)
