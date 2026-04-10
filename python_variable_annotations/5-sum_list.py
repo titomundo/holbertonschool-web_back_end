a1b2815f117f2504ce13839321de2b2bc13ec2cd
titomundo <tito.alberto.mundo@gmail.com>
2026-04-09 21:25:56 -0600

    import List from typings

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -4,8 +4,10 @@
 Get the sum of a list of floats
 """
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """
     Returns the sum of a list of floats
 
