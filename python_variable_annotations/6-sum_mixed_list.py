8e96ea6e6aa7e4f645a1e962d0c690fd2b7f989b
titomundo <tito.alberto.mundo@gmail.com>
2026-04-10 23:40:12 -0600

    ints and floats

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -6,7 +6,7 @@ Return the sum of a mixed bag of numbers"""
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """
     Returns the sum of a list of floats and integers
 
