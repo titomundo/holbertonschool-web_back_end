d850132b00de2b5c4ec1a9c20b3de23165f59f2c
titomundo <tito.alberto.mundo@gmail.com>
2026-04-16 01:11:16 -0600

    task 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""1-concurrent_coroutines
+Create many async calls
+"""
+
+from typing import List
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Creates many instances of wait_random
+
+    Args:
+    n (int): number of instances
+    max_delay: maximun number of seconds of delay
+    Returns:
+    list[float]: sorted list of all delays
+    """
+    delays = []
+    for i in range(n):
+        delay = await wait_random(max_delay)
+        delays.append(delay)
+
+    for i in range(len(delays)):
+        for j in range(i + 1, len(delays)):
+            if delays[i] > delays[j]:
+                delays[i], delays[j] = delays[j], delays[i]
+
+    return delays
