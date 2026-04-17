7b15f969fa27bb5f2ce57384a25379b358d8dd11
titomundo <tito.alberto.mundo@gmail.com>
2026-04-16 23:13:31 -0600

    task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""0-async_generator
+Create a coroutine that generates random numbers each second
+"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Yields random numbers from 0 to 10 every second
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
