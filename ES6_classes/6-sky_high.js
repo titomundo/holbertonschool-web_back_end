ba41125bb2f6554619c9ebe62988635b9a388737
titomundo <tito.alberto.mundo@gmail.com>
2026-05-03 21:08:04 -0600

    task 6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,28 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return super.sqft;
+  }
+
+  set sqft(sqft) {
+    super.sqft = sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    this._floors = floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
