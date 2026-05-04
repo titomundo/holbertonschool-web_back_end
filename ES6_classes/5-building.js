ee36ce8cfbeb56fba1926813cef4b6a76eff07ca
titomundo <tito.alberto.mundo@gmail.com>
2026-05-03 20:53:13 -0600

    check for inhertance

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,7 +2,7 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
 
-    if (!(this.prototype instanceof Building) && !Object.hasOwn(this, 'evacuationWarningMessage')) {
+    if (this.constructor !== Building && !Object.hasOwn(this, 'evacuationWarningMessage')) {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
   }
