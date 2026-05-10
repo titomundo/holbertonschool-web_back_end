d49603d3f86e42ed8555406b40d6cb1e1e75eb92
titomundo <tito.alberto.mundo@gmail.com>
2026-05-09 23:30:26 -0600

    task 7

---
+++ ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,9 @@
+export default function hasValuesFromArray(set, arr) {
+  for (let i = 0; i < arr.length; i += 1) {
+    if (!set.has(arr[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
