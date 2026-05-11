87883487ae51b08bd3780db9f9e36ea0adeea395
titomundo <tito.alberto.mundo@gmail.com>
2026-05-10 19:08:06 -0600

    task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,20 @@
+export default function cleanSet(set, startString) {
+  let str = '';
+
+  if (startString === '') {
+    return '';
+  }
+
+  const arr = Array.from(set);
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i].startsWith(startString)) {
+      if (i + 1 <= arr.length && i > 0) {
+        str += '-';
+      }
+
+      str += arr[i].replace(startString, '');
+    }
+  }
+
+  return str;
+}
