1a21945964c501b74e14bf1defd8d49a6d97b848
titomundo <tito.alberto.mundo@gmail.com>
2026-05-10 19:10:31 -0600

    check for no string or non string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   let str = '';
 
-  if (startString === '') {
+  if (startString === '' || (typeof startString !== 'string')) {
     return '';
   }
 
