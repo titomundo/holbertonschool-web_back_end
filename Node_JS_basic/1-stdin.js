baa60179b5eccc00a14c5876075c6abde6763091
titomundo <tito.alberto.mundo@gmail.com>
2026-05-13 15:42:15 -0600

    task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question(
+  'Welcome to Holberton School, what is your name?\n',
+  (name) => {
+    console.log(`Your name is: ${name}`);
+    rl.close();
+  },
+);
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
