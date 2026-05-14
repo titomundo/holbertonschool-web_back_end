110b5ad8662b1aa59c2c0ff2a0ef5416eaa678b6
titomundo <tito.alberto.mundo@gmail.com>
2026-05-13 22:18:52 -0600

    use process.stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,13 @@
-const readline = require('node:readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on('readable', () => {
+  const buff = process.stdin.read();
+  if (buff !== null) {
+    process.stdout.write(`Your name is: ${buff}`);
+  }
 });
 
-rl.question(
-  'Welcome to Holberton School, what is your name?\n',
-  (name) => {
-    console.log(`Your name is: ${name}`);
-    rl.close();
-  },
-);
-
-rl.on('close', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing');
 });
